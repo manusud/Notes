32b76481e18ccbecff338dc1267a5bd6975c1af8
Luiz Manoel <luiz.costa@magazineluiza.com.br>
2020-12-09 23:28:52 -0300

    Adicionar arquivos de projeto.

---
+++ Notes/Notes/NotesPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System.IO;
+using Notes.Models;
+
+namespace Notes
+{
+    public partial class NotesPage : ContentPage
+    {
+        public NotesPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            listView.ItemsSource = await App.Database.GetNotesAsync();
+        }
+
+        async void OnNoteAddedClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new NoteEntryPage
+            {
+                BindingContext = new Note()
+            });
+        }
+
+        async void OnListViewItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem != null)
+            {
+                await Navigation.PushAsync(new NoteEntryPage
+                {
+                    BindingContext = e.SelectedItem as Note
+                });
+            }
+        }
+    }
+}
